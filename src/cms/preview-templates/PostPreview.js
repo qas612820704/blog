a4c645a93ccb378ebbfe20322c10bdb5fcb6ef92
Lego <qas612820704@gmail.com>
2019-01-22 10:36:56 +0800

    Add netlify CMS post preview styles

--- src/cms/preview-templates/PostPreview.js
+++ src/cms/preview-templates/PostPreview.js
@@ -1,26 +1,33 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import moment from 'moment';
+
 import Post from '../../components/Post';
+import typography from '../../layouts/typography';
+import '../../layouts/theme.css';
+
 
 
 const PostPreview = ({ entry, widgetFor }) => (
-  <Post active isNetlifyCMS
-    post = {{
-      html: widgetFor('body'),
-      timeToRead: 0,
-      excerpt: '',
-      frontmatter: {
-        title: entry.getIn(['data', 'title']),
-        cover: '',
-        date: moment(entry.getIn(['data', 'date'])).format("MMM D, YYYY"),
-        category: entry.getIn(['data', 'category']),
-        tags:  entry.getIn(['data', 'tags']),
-      },
-      fields: {
-        slug: '/', // TODO
-      }
-    }}
-  />
+  <Fragment>
+    <style>{typography.toString()}</style>
+    <Post active isNetlifyCMS
+      post = {{
+        html: widgetFor('body'),
+        timeToRead: 0,
+        excerpt: '',
+        frontmatter: {
+          title: entry.getIn(['data', 'title']),
+          cover: '',
+          date: moment(entry.getIn(['data', 'date'])).format("MMM D, YYYY"),
+          category: entry.getIn(['data', 'category']),
+          tags:  entry.getIn(['data', 'tags']),
+        },
+        fields: {
+          slug: '/', // TODO
+        }
+      }}
+    />
+  </Fragment>
 )
 
 export default PostPreview;
